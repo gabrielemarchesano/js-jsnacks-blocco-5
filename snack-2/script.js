5a6131495979f46bdbf4c529a3f494fff4ef9543
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-22 13:55:56 +0200

    snack2: use for loop, forEach method and dot notation to access object value

--- snack-2/script.js
+++ snack-2/script.js
@@ -5,4 +5,16 @@ const people = [
 ];
 
 // Stampa in console tutti i nomi
+
+// Ciclo for
+console.log("Ciclo for ⤵");
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+  console.log(person.name);  
+}
+
+// Metodo forEach()
+console.log("Metodo forEach() ⤵");
+people.forEach((person) => console.log(person.name))
+
 // Risultato: 'Paolo', 'Giulia', 'Marco'
