0155abcc773d1191c37e311b615be9022afe40c4
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-22 13:49:06 +0200

    snack1: use for loop and forEach method to print

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,15 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+// Ciclo for
+console.log("Ciclo for ⤵");  
+for (let i = 0; i < names.length; i++) {
+  const name = names[i];
+  console.log(name);  
+}
+
+// Metodo forEach()
+console.log("Metodo forEach() ⤵");
+names.forEach((name) => console.log(name))
+
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
