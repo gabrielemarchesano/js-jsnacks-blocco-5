a35c6402ac61dbfd74249e45125ec41c39935181
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-22 14:58:52 +0200

    snack8: use for loop, forEach and find methods

--- snack-8/script.js
+++ snack-8/script.js
@@ -20,4 +20,30 @@ const students = [
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
+
+// Ciclo for
+console.log("Ciclo for ⤵");
+
+for (let i = 0; i < students.length; i++) {
+  const student = students[i];
+  if(student.name === "Marco Lanci"){
+    console.log(student.class);    
+  }
+}
+
+// Metodo forEach()
+console.log("Metodo forEach() ⤵");
+
+students.forEach((student) => {
+  if(student.name === "Marco Lanci"){
+    console.log(student.class);    
+  }
+})
+
+// Metodo find()
+console.log("Metodo find() ⤵");
+
+const studentClass = students.find((student) => student.name === "Marco Lanci");
+console.log(studentClass.class);
+
 // Risultato: '3C'
