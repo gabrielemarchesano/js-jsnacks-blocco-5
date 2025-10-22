774a227742392558d9f321dff710cb05e0f0d1bf
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-22 14:16:17 +0200

    snack4: use for loop, forEach and map methods

--- snack-4/script.js
+++ snack-4/script.js
@@ -6,4 +6,30 @@ const posts = [
 ]
 
 // Crea un nuovo array coi nomi di tutti gli autori
+
+// Ciclo for
+console.log("Ciclo for ⤵");
+
+const authorNames = [];
+
+for (let i = 0; i < posts.length; i++) {
+  const post = posts[i];
+  authorNames.push(post.author);
+}
+console.log(authorNames);
+
+// Metodo forEach()
+console.log("Metodo forEach() ⤵");
+
+const names = [];
+
+posts.forEach((post) => names.push(post.author))
+console.log(names);
+
+// Metodo map()
+console.log("Metodo map() ⤵");
+
+const authors = posts.map((post) => post.author) 
+console.log(authors);
+
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
